5f0c88a630e1b3434917f8036b61ae80076edeed
parth81220 <parthchauhan812@gmail.com>
2024-06-12 10:43:58 +0530

    nginx erros

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/conf.d
+sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/jenkins-django/app
 
-sudo ln -s etc/nginx/sites-available /etc/nginx/sites-enabled
+sudo ln -s etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
